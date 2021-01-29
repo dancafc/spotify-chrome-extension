173682d7e2bda787bdec3e302d93e523d2b4c4d2
Dan <daniel_roth@hotmail.co.uk>
2021-01-29 17:26:47 +0000

    testing

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     var checkPageButton = document.getElementById('checkPage');
     checkPageButton.addEventListener('click', function(){
-        alert("hello BITCH!");
+        alert("hello BITCH! your spotify sucks");
     });
 });
